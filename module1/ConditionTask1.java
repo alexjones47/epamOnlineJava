64fb0a06dc3c4c9359fbdb1a50174a9b7f3b9785
Melnikov Aleksei <57940714+alexjones47@users.noreply.github.com>
2020-03-07 11:03:24 +0200

    Module 1 - Basics

---
+++ module1/ConditionTask1.java
@@ -0,0 +1,31 @@
+package by.javaonline.module1;
+
+/*
+   Даны два угла треугольника (в градусах). Определить, существует ли такой
+   треугольник, и если да, то будет ли он прямоугольный.
+*/
+
+public class ConditionTask1 {
+    public static void main(String[] args) {
+
+        double firstAngle;
+        double secondAngle;
+        boolean isTriangleExist;
+        boolean isTriangleRight;
+
+        firstAngle = 137.8;
+        secondAngle = 37.3;
+        isTriangleExist = false;
+        isTriangleRight = false;
+
+        if ((firstAngle + secondAngle) < 180) {
+            isTriangleExist = true;
+            if ((firstAngle + secondAngle) == 90 || firstAngle == 90 || secondAngle == 90 ) {
+                isTriangleRight = true;
+            }
+        }
+
+        System.out.println("Существует ли такой треугольник? " + isTriangleExist);
+        System.out.println("Является ли этот треугольник прямоугольным? " + isTriangleRight);
+    }
+}
