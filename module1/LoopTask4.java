8e61a6507464fe552ae3eec3fd2215dc66d7f0f1
Melnikov Aleksei <57940714+alexjones47@users.noreply.github.com>
2020-03-22 12:16:50 +0200

    Module 1 - Basics

---
+++ module1/LoopTask4.java
@@ -0,0 +1,24 @@
+package by.javaonline.module1;
+/*
+    Составить программу нахождения произведения квадратов первых двухсот чисел.
+*/
+
+public class LoopTask4 {
+
+    public static void main(String[] args) {
+
+        double mult;
+
+        mult = 1;
+
+        for (int i = 1; i <= 200; i++) {
+            mult = mult * Math.pow(i, 2);
+        }
+        if (Double.isInfinite(mult)) {
+            System.out.println("Значение произведения выходит за пределы диапазона значений примитивного типа данных double");
+
+        } else {
+            System.out.println("Произведение квадратов первых двухсот чисел: " + mult);
+        }
+    }
+}
