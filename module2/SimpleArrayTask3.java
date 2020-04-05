3ddaf7846d67ce12162b56076f8d88b4ac28cf9b
Melnikov Aleksei <57940714+alexjones47@users.noreply.github.com>
2020-04-05 19:02:13 +0200

    Module 2 - Algoritmization

--- module2/SimpleArrayTask3.java
+++ module2/SimpleArrayTask3.java
@@ -7,28 +7,41 @@ package by.javaonline.module2;
 */
 
 import java.util.Scanner;
+import java.util.Random;
 
 public class SimpleArrayTask3 {
 
     public static void main(String[] args) {
-
-        int countNegative = 0;
-        int countPositive = 0;
-        int countZero = 0;
-
+        
         System.out.print("Введите размерность массива N: ");
 
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-
+        
         double[] array = new double[n];
+        
+        initArray(array);
 
+        analyzeArray(array);
+    }
+
+    public static void initArray(double[] array) {
+        if (array == null) {
+            return;
+        }
+        Random random = new Random();
         System.out.println("\nЗаданный массив:");
-
-        for (int i = 0; i < array.length; i++) {
-            array[i] = (int)(Math.random()*100 - 50);
+        for (int i = 0; i < array.length; i++){
+            array[i] = random.nextInt(100) - 50;
             System.out.print(array[i] + "   ");
         }
+    }
+
+    public static void analyzeArray(double[] array) {
+
+        int countNegative = 0;
+        int countPositive = 0;
+        int countZero = 0;
 
         for (int i = 0; i < array.length; i++) {
 
@@ -44,6 +57,6 @@ public class SimpleArrayTask3 {
         System.out.println("\n\nКоличество положительных элементов массива: " + countPositive);
         System.out.println("Количество отрицательных элементов массива: " + countNegative);
         System.out.println("Количество элементов массива равных нулю: " + countZero);
-
     }
+
 }
